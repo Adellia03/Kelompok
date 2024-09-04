3a1234ec07dd99f50a217d35f7896d2ac7f23ab8
Adellia03 <salsadellia736@gmail.com>
2024-09-04 11:51:24 +0700

    Add files via upload

---
+++ Latihan03.java
@@ -0,0 +1,18 @@
+public class Latihan03 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Adellia Salsa Al Barra");
+        System.out.println("NIM      : 244107020222");
+        System.out.println("Kelas    : 1B");
+        System.out.println("No Absen : 03");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : Karangjuwet Rt:32 Rw:08 Karangploso Malang");
+        System.out.println("No HP    : 085162931063");
+        System.out.println("Hobby    : Memasak");
+
+    }
+}
